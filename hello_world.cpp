fee716905caf0cfe245c5f06ee049ea8cce17f99
vilkafad <violetta_fadeevaaa@mail.ru>
2025-11-12 23:10:50 +0300

    style: apply Mozilla code style with clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
 
+// Main function that greets user! Now with punctuation.
 // Основная функция, которая приветствует пользователя!
-int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;  // Program finished successfully
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
+  return 0; // Program finished successfully
 }
+
