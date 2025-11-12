6447bc0ba29627ffa2f0671a57a1ca8e680584b1
vilkafad <violetta_fadeevaaa@mail.ru>
2025-11-12 22:51:22 +0300

    docs: add code comments for better readability

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-int main () {
-std::string name;
- std::cout<< "Enter your name: ";
-std::cin<<name;
-std::cout<<"Hello world from " << name << std::endl;
-return 0;
+
+// Main function that greets user
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;  // Program finished successfully
 }
