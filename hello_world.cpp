b701d181cf492e37bbc3190151e7d0c8d139d1e5
vilkafad <violetta_fadeevaaa@mail.ru>
2025-11-12 22:08:05 +0300

    feat: add user input for personalized greeting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
-int main { 
-cout << "Hello world" << endl;
+int main() {
+string name;
+cout << "Enter your name: ";
+cin >> name;
+cout << "Hello world from " << name<<endl;
 return 0;
 }
